b78e6fc8c5dc25f1630263efe5e6db725511f90b
Victor Parga <victorparga765@gmail.com>
2023-09-19 18:42:51 -0300

    Layout Responsivo

--- seu-script.js
+++
@@ -1,6 +0,0 @@
-<script>
-    window.addEventListener("scroll", function () {
-        var header = document.querySelector(".header");
-        header.classList.toggle("scroll", window.scrollY > 0);
-    });
-</script>
